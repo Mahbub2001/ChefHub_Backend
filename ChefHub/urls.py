439d7f4ff99305f2fe96623d0dd1283c37d20b2c
Mahbub2001 <imahbubturza@gmail.com>
2024-05-21 00:54:33 +0600

    update_url

--- ChefHub/urls.py
+++ ChefHub/urls.py
@@ -12,9 +12,6 @@ class CustomAPIRoot(APIView):
     permission_classes = [AllowAny] 
     def get(self, request, *args, **kwargs):
         return Response({
-            'register': reverse('register', request=request),
-            'login': reverse('login', request=request),
-            # 'logout': reverse('logout', request=request),
             'recipelist': reverse('public-recipe-list', request=request),
             'eventlist': reverse('public-event-list', request=request),
         })
@@ -29,7 +26,6 @@ urlpatterns = [
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('recipelist/', PublicRecipeListAPIView.as_view(), name='public-recipe-list'),
     path('eventlist/', PublicEventListAPIView.as_view(), name='public-event-list'),
-    # Define the empty path to point to the custom API root view
     path('', CustomAPIRoot.as_view(), name='api-root'),
 ]
 
