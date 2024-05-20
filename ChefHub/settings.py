8793a481378dc58f29b1beb3bab82bbaaa17e687
Mahbub2001 <imahbubturza@gmail.com>
2024-05-21 00:33:50 +0600

    updateurl

--- ChefHub/settings.py
+++ ChefHub/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://chefhub-backend.onrender.com/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://chefhub-backend.onrender.com','https://*.127.0.0.1']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
