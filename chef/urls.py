09f34173cbdbf56464dc90abc6b29a3b9f6a7eea
Mahbub2001 <imahbubturza@gmail.com>
2024-05-21 01:05:28 +0600

    update_url

--- chef/urls.py
+++ chef/urls.py
@@ -9,7 +9,7 @@ router.register(r'profile', views.ChefProfileViewSet, basename='chef-profile')
 urlpatterns = [
     path('', include(router.urls)),
     path('register/', views.UserRegistrationApiView.as_view(), name='register'),
-    # path('login/', views.UserLoginApiView.as_view(), name='login'),
-    # path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('login/', views.UserLoginApiView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('active/<uid64>/<token>/', views.activate, name='activate'),
 ]
